45032385e2fb6ecec4c538f8da97d65de92b4b4f
koleaabramovici <78415812+koleaabramovici@users.noreply.github.com>
2021-03-18 17:26:35 +0200

    My seccond JS exercise

---
+++ dog.js
@@ -0,0 +1,19 @@
+let myAge = 23;//This is my age.
+
+let earlyYears = 2;//Assigned the value of 2 to earlyYears.
+earlyYears = earlyYears * 10.5;
+
+
+let laterYears = myAge - 2;//Substracted 2 from my age and assigned the result to a new variable.
+laterYears = laterYears * 4;//Calculating the number of dog years accounted for by my later years.
+
+const myAgeInDogYears = earlyYears + laterYears;//Added the values of two variables to find out my age in dog years.
+const myName = 'Nicolae'.toLowerCase();//Assigned the value of my name and added the .toLowerCase method.
+console.log(`My name is ${myName}. I'm ${myAge} years old in human years which is ${myAgeInDogYears} years in dog years.`);
+
+
+
+
+
+
+
